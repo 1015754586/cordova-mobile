115f62168f7dda57c28eb206dac3b16c92cd50c7
tianle <tianle@redhat.com>
2015-11-11 18:27:53 +0800

    update many features.

--- www/js/errata.js
+++ www/js/errata.js
@@ -14,13 +14,16 @@ var Errata = function(){
 	this.detail_page = $('#detail_page');
 	this.login_page = $('#login_page');
 	this.back_page;
+	this.loading = $("#loading");
+	this.loginname = $("#u_login_name");
+	this.realname = $("#u_real_name");
 	
 	this.homeinit = false;
 	this.listinit = false;
 	this.chartinit = false;
 	this.myinit = false;
 	
-	this.user = {};
+	this.user;
 	this.loginState = true;
 	
 	$.support.cors = true;
@@ -64,55 +67,74 @@ function Advisory(data){
 
 function User(data){
 	this.id = data.id;
-	this.loginName = data.loginname;
-	this.realName = data.realname;
+	this.loginname = data.loginname;
+	this.realname = data.realname;
 	this.role = data.role;
 }
 
 Errata.prototype.Errata = Errata;
 
 Errata.prototype.init = function(type){	
-	
-	$.mobile.loading( "show", {
-	            text: '',
-	            textVisible: '',
-	            theme: '',
-	            textonly: '',
-				html: ''
-	});
-	
-	$('#changeqeowner').toggle(function() {
-	  $('#qeowner').fadeIn();
-	},function(){$('#qeowner').fadeOut();});
+	this.initDB();
+	$.mobile.loading( "show", {text: '',textVisible: '',theme: '',textonly: '',html: ''});
+	$('#changeqeowner').toggle(function() {$('#qeowner').fadeIn();},function(){$('#qeowner').fadeOut();});
 
+	// this.removeUser();
 	this.hideAllPage();
-	
 	var errata = this;
-	
-	$('#footer_home').bind("tap",function(){
-		errata.activeFooter($(this));
-		errata.showPage(errata.home_page);
+		
+	$('#footer_home').bind("click",function(){
+		errata.tabNavigate($(this),errata.home_page);
 		errata.home();
 	});
-	$('#footer_list').bind("tap",function(){
-		errata.activeFooter($(this));
-		errata.showPage(errata.list_page);
+	$('#footer_list').bind("click",function(){
+		errata.tabNavigate($(this),errata.list_page);
 		errata.list();
 	});
-	$('#footer_chart').bind("tap",function(){
-		errata.activeFooter($(this));
-		errata.showPage(errata.chart_page);
+	$('#footer_chart').bind("click",function(){
+		errata.tabNavigate($(this),errata.chart_page);
 		errata.chart();
 	});
-	$('#footer_my').bind("tap",function(){
-		errata.activeFooter($(this));
-		errata.showPage(errata.my_page);
-		// errata.my();
+	$('#footer_my').bind("click",function(){
+		errata.tabNavigate($(this),errata.my_page);
+		errata.my();
 	});
-	// this.checkLogin();
-// 	var errata = this;
-	this.home();
+	this.checkLogin();
+	// this.home();
 	navigator.splashscreen.hide();
+};
+
+Errata.prototype.initLogin = function(){
+	var errata = this;
+	if(this.loginState){
+		this.user = this.getUser();
+		this.home();
+	}else{
+		this.footer.hide();
+		this.login_page.show();
+		$('#signin').click(function(){
+			errata.login($('#loginID').val());
+		});
+	}
+};
+
+Errata.prototype.login = function(name){
+	var errata = this;
+	$.ajax({
+		url : this.USER_API,
+		data : {username : name},
+		dataType : "jsonp",
+		success : function(data){
+			var user = new User(data);
+			errata.saveUser(user);
+			errata.login_page.hide();
+			errata.footer.show();
+			errata.home();
+		},
+		error : function(xhr,status,err){
+			alert(status);
+		}
+	});
 };
 
 Errata.prototype.home = function(){
@@ -145,6 +167,8 @@ Errata.prototype.chart = function(type){
 
 Errata.prototype.my = function(type){
 	if(! this.myinit){
+		this.loginname.text(this.user.loginname);
+		this.realname.html(this.user.realname+"<br>"+this.user.role);
 		this.myinit = true;
 	}
 	this.back_page = this.my_page;
@@ -199,29 +223,10 @@ Errata.prototype.search = function(searchStr){
 	
 };
 
-Errata.prototype.login = function(name){
-	// alert(name);
-	var errata = this;
-	$.ajax({
-		url : this.USER_API,
-		data : {username : name},
-		dataType : "jsonp",
-		success : function(data){
-			alert(data);
-			var user = new User(data);
-			errata.saveUser(user);
-			errata.login_page.hide();
-			errata.home();
-		},
-		error : function(xhr,status,err){
-			alert(status);
-		}
-	});
-};
-
 Errata.prototype.detail = function(id){
-	this.showPage(this.detail_page);
+	this.showPage(this.detail_page,'slide');
 	var errata = this;
+	errata.loading.show();
 	$.ajax({
 		url : this.DETAIL_API,
 		data : {id : id},
@@ -232,6 +237,7 @@ Errata.prototype.detail = function(id){
 			$('#back_icon').click(function(){
 				errata.turnBack();
 			});
+			errata.loading.hide();
 		},
 		error : function(xhr,status,err){
 			alert(status);
@@ -260,85 +266,102 @@ Errata.prototype.comment = function(id,content){
 	
 };
 
+// /**
+// * SQLite operation
+// */
 Errata.prototype.saveUser = function(user){
-	var db = window.sqlitePlugin.openDatabase({name: "my.db", location: 1});
-	
+	var db = this.db();
 	var errata = this;
 	db.transaction(function(tx) {
-      tx.executeSql("INSERT INTO t_user (userId, loginname, realname,role) VALUES (?,?,?,?)", [user.id,user.loginname,user.realname,role], 
+      tx.executeSql("INSERT INTO t_user (userId, loginname, realname,role) VALUES (?,?,?,?)", [user.id,user.loginname,user.realname,user.role],
 		function(tx, res) {
 			errata.user = user;
       });
 	});
 };
 
-Errata.prototype.getUser = function(){
-	var db = window.sqlitePlugin.openDatabase({name: "my.db", location: 1});
-	
-	var data = {};
+Errata.prototype.removeUser = function(user){
+	var db = this.db();
+
 	var errata = this;
 	db.transaction(function(tx) {
-      tx.executeSql("select userId as id,loginname,realname,role from t_user;", [], function(tx, res) {
-		 var count = res.rows.length;
-		 if(count ==1){
-			 data.id = res.rows.item(0).id; 
-			 data.loginname = res.rows.item(0).loginname; 
-			 data.realname = res.rows.item(0).realname; 
-			 data.role = res.rows.item(0).role; 
-			 this.user = User(data);
-		 }
-      });
-	  });
+      // tx.executeSql("delete from t_user where userId = ? ", [user.id],
+// 		function(tx, res) {
+//
+//       });
+	tx.executeSql("delete from t_user");
+	});
+};
+
+Errata.prototype.getUser = function(){
+	var db = this.db();
+	var data = {};
+	var errata = this;
+	if(errata.user == 'undefined' || errata.user == null){
+		db.transaction(function(tx) {
+	      tx.executeSql("select userId as id,loginname,realname,role from t_user;", [], function(tx, res) {
+			 var count = res.rows.length;
+			 if(count ==1){
+				 data.id = res.rows.item(0).id;
+				 data.loginname = res.rows.item(0).loginname;
+				 data.realname = res.rows.item(0).realname;
+				 data.role = res.rows.item(0).role;
+				 errata.user = new User(data);
+			 }
+	      });
+		});
+	}
 };
 
 Errata.prototype.checkLogin = function(){
-	// alert(5);
-	var db = window.sqlitePlugin.openDatabase({name: "my.db", location: 1});
-	
-	var count = 0;
+	var db = this.db();
 	var errata = this;
 	db.transaction(function(tx) {
       tx.executeSql("select count(id) as cnt from t_user;", [], function(tx, res) {
-		  count = res.rows.length;
-		  alert(count);
-		  if(count == 1){
+		  if(res.rows.item(0).cnt == 1){
 			  errata.loginState = true;
+			  errata.initLogin();
 		  }else{
 		  	errata.loginState = false;
+			errata.initLogin();
 		  }
       });
-	  });
-	
-	  if( count == 1){
-		  return true;
-	  }else{
-	  	  return false;
-	  }
+	});
 };
 
-Errata.prototype.initLogin = function(){
-	alert(8);
-	var errata = this;
-	alert(errata.loginState);
-	if(this.loginState){
-		alert('Y');
-		this.user = this.getUser();
-		this.home();
-	}else{
-		this.login_page.css('display','block');
-		$('#signin').click(function(){
-			errata.login($('#loginID').val());
-		});
-	}
-	
+Errata.prototype.initDB = function(){
+	var db = this.db();
+	db.transaction(function(tx) {
+    	tx.executeSql('CREATE TABLE IF NOT EXISTS t_user (id integer primary key,userId integer , loginname text, realname text,role text)');
+ 	});
 };
 
-Errata.prototype.showPage = function(errata_page){
+Errata.prototype.db = function(){
+	var db = window.sqlitePlugin.openDatabase({name: "my.db", location: 1});
+	return db;
+};
+
+Errata.prototype.dropTable = function(){
+	var db = window.sqlitePlugin.openDatabase({name: "my.db", location: 1});
+	db.transaction(function(tx) {
+    	tx.executeSql('DROP TABLE IF EXISTS t_user');
+ 	});
+};
+/* SQLite operation end*/
+
+Errata.prototype.showPage = function(errata_page,type){
 	this.hideAllPage();
-	$('.header').css('position','static');
-	var page_div = errata_page.find("div[class='header']").next();
-	page_div.css('margin-top', '0px');
-	errata_page.show('slide',{direction : 'right'},300,function(){$('.header').css('position','fixed');page_div.css('margin-top', '64px');});
+	if(type == 'slide'){
+		$('.header').css('position','static');
+		var page_div = errata_page.find("div[class='header']").next();
+		page_div.css('margin-top', '0px');
+		errata_page.show('slide',{direction : 'right'},300,function(){
+			$('.header').css('position','fixed');
+			page_div.css('margin-top', '64px');
+		}); 
+	}else{
+		errata_page.show();
+	}
 };
 
 Errata.prototype.hideAllPage = function(){
@@ -364,4 +387,22 @@ Errata.prototype.activeFooter = function(footer){
 	$('#footer_chart').removeClass('active');
 	$('#footer_my').removeClass('active');
 	footer.addClass('active');
+};
+
+Errata.prototype.tabNavigate = function(tab,page){
+	this.activeFooter(tab);
+	this.showPage(page);
+};
+
+Errata.prototype.pushRegister = function(){
+	var username = this.user.username;
+	var pushConfig = {
+	      pushServerURL: "https://aerogear-pntdev.rhcloud.com/ag-push/",
+		  alias : username,
+	      ios: { 
+	        variantID: "46734504-529e-47b1-a9d8-93ca5b68d3c1",
+	        variantSecret: "27ecc4a7-b262-44e0-9943-ab032479e59d"
+	      }
+	};
+	push.register(app.onNotification, successHandler, errorHandler, pushConfig);
 };
--- www/js/index.js
+++ www/js/index.js
@@ -16,35 +16,19 @@ var app = {
   // function, we must explicitly call 'app.receivedEvent(...);'
   onDeviceReady: function() {
 	  // alert(device.platform+","+device.cordova);
-	// var db = window.sqlitePlugin.openDatabase({name: "my.db", location: 1});
-//
-// 	db.transaction(function(tx) {
-// 	    //tx.executeSql('DROP TABLE IF EXISTS t_user');
-// 	   //tx.executeSql('CREATE TABLE IF NOT EXISTS t_user (id integer primary key,userId integer , loginname text, realname text,role text)');
-//
-// 	    // tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
-// // 		  alert(res.insertId);
-// //
-// // 	      tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
-// // 			alert(res.rows.item(0).cnt);
-// // 	      });
-// //
-// // 	    }, function(e) {
-// // 	      console.log("ERROR: " + e.message);
-// // 	    });
-// 	  });
-		
+
 	var errata = new Errata();
     app.receivedEvent('deviceready');
 	var pushConfig = {
 	      pushServerURL: "https://aerogear-pntdev.rhcloud.com/ag-push/",
+		  alias : "ltian@redhat.com",
 	      ios: { 
 	        variantID: "46734504-529e-47b1-a9d8-93ca5b68d3c1",
 	        variantSecret: "27ecc4a7-b262-44e0-9943-ab032479e59d"
 	      }
 	    };
-push.register(app.onNotification, successHandler, errorHandler, pushConfig);
-
+	push.register(app.onNotification, successHandler, errorHandler, pushConfig);
+	
 function successHandler() {
   //alert('success')
 }
